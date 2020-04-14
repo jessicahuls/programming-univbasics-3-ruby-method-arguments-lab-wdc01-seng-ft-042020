1db0918f3ae4ca7a3565d3df2b475dc03c018d66
Jessica Huls <jessica.i.huls@gmail.com>
2020-04-14 14:24:04 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,11 +8,10 @@ def introduction_with_language(name, language)
 end
 introduction_with_language("Jessica", "Ruby")
 
-language = "Ruby"
-def introduction_with_language_optional(name, language)
+def introduction_with_language_optional(name, language = "Ruby")
 print "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
-introduction_with_language_optional("Edwin", language = nil )
+
 
 
 
